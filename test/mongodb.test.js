2801c5595362696b71303fc91d6134bda2d16d4c
ThomasKiszely <thomas@bohn-jacobsen.dk>
2025-11-21 11:19:50 +0100

    test af mongodb

--- test/mongodb.test.js
+++ test/mongodb.test.js
@@ -1,26 +1,29 @@
-import { describe, vi, expect, it } from 'vitest';
-import mongoose from 'mongoose';
+import { describe, it, expect, beforeAll, afterAll } from "vitest";
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const mongoose = require("mongoose");
+const { connectToMongo } = require("../src/services/db.js");
 
-import { connectToMongo } from 'src/services/db';
+let mongoServer;
 
-vi.mock("mongoose", () => {
-    return {
-        default: {
-            connect: vi.fn(),
-            connection: {
-                on: vi.fn(),
-                once: vi.fn(),
-                close: vi.fn(),
-            },
-        },
-    };
+beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create();
+    process.env.DB_URL = mongoServer.getUri();
 });
 
-describe("connectToMongo", () => {
-    it('Should call mongoose.connectToMongo with db_url', async () => {
-        process.env.DB_URL = "mongodb://localhost:27017/testdb";
-        await connectToMongo();
-        expect(mongoose.connect(process.env.DB_URL)).toHaveBeenCalledWith(process.env.DB_URL);
-    });
-})
+afterAll(async () => {
+    await mongoose.connection.close();
+    await mongoServer.stop();
+});
 
+describe("MongoDB connection", () => {
+    it("connects to MongoDB", async () => {
+        connectToMongo();
+
+        await new Promise((resolve, reject) => {
+            mongoose.connection.once("open", resolve);
+            mongoose.connection.once("error", reject);
+        });
+
+        expect(mongoose.connection.readyState).toBe(1);
+    });
+});
