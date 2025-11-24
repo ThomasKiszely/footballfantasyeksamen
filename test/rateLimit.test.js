29f716589413e8027ec7c42a686175b2d6cf62d4
kongkambyy <joachimkambyn@gmail.com>
2025-11-24 11:29:19 +0100

    user routes test

--- test/rateLimit.test.js
+++ test/rateLimit.test.js
@@ -15,10 +15,21 @@ describe('Rate Limiter Middleware', () => {
         app = express();
         app.use(rateLimiter);
         app.get('/test', (req, res) => {
-            res.status(200).json({ message: 'success' });
+            res.status(200).json({message: 'success'});
         });
     });
 
-    it
+    it('Should allow request if not exceeded the limit', async () => {
+        const res = await request(app).get('/test');
+        expect(res.status).toBe(200);
+    });
 
-}
+    it('Should block requests if limit is exceeded', async () => {
+        for (let i = 0; i < 100; i++) {
+            await request(app).get('/test');
+        }
+        const res = await request(app).get('/test');
+        expect(res.status).toBe(429);
+    });
+});
+
