e78e6396182653077b0767d0c11faa425ae7038c
ThomasKiszely <thomas@bohn-jacobsen.dk>
2025-11-27 11:46:00 +0100

    bruger ændret....

--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,7 @@ const { roles } = require('../utils/roles');
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    teams: { type: [ String ] },
+    teams: { type: [ String ], default: [] },
     role: { type: String, enum: roles, default: "user", required: true },
 });
 
