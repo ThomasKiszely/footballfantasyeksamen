108838349908055a39a06b540674a77f7cc8c3bf
SpeedosDK <88831904+SpeedosDK@users.noreply.github.com>
2025-12-09 12:59:53 +0100

    api test bug fixede

--- test/API.test.js
+++ test/API.test.js
@@ -1,12 +1,22 @@
+/**
+* @vitest-environment node
+  */
+require('dotenv').config();
 import {describe, it, expect, vi} from "vitest";
 import axios from 'axios';
+import {builtinEnvironments} from "vitest/environments";
+const apiKey = process.env.API_FOOTBALL_KEY
+
 
 describe('Live API test', () => {
     it('Fetches data from real life football matches', async () => {
-        const response = await axios.get('https://api.football-data.org/v4/competitions/PL/matches');
+        const response = await axios.get('https://api.football-data.org/v4/competitions/PL/matches', {
+            headers: {'X-Auth-Token': apiKey},
+        });
+        const matchData = response.data.matches;
         expect(response.status).toBe(200);
-        expect(Array.isArray(response.data)).toBe(true);
-        expect(response.data.length).toBeGreaterThan(0);
-        expect(response.data[0]).toHaveProperty('name');
+        expect(Array.isArray(matchData)).toBe(true);
+        expect(matchData.length).toBeGreaterThan(0);
+        expect(matchData[0]).toHaveProperty('id');
     });
 });
